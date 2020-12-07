a3614376be558a5f8ee3c017026b0b3846d58944
prasantabarik <54621979+prasantabarik@users.noreply.github.com>
2020-12-07 13:42:07 +0100

    Update ServiceKotlinTemplateApplication.kt

--- src/main/kotlin/com/tcs/service/ServiceKotlinTemplateApplication.kt
+++ src/main/kotlin/com/tcs/service/ServiceKotlinTemplateApplication.kt
@@ -2,10 +2,15 @@ package com.tcs.service
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import khttp.get
 
 @SpringBootApplication
 class ServiceKotlinTemplateApplication
 
 fun main(args: Array<String>) {
     runApplication<ServiceKotlinTemplateApplication>(*args)
+    //get(url = "http://localhost:3500/v1.0/secrets/azurekeyvault/deliverymomentdbapi")
+    println(get(url = "http://localhost:3500/v1.0/secrets/azurekeyvault/deliverymomentdbapi?metadata.namespace=edppublic-deliverymomentcrud-dev"))
+    println(get(url = "http://localhost:3500/v1.0/secrets/azurekeyvault/deliverycruddb?metadata.namespace=edppublic-deliverymomentcrud-dev"))
+    
 }
